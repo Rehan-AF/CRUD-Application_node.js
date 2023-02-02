d7f7aa62c6af815b87de7929a7b5989267b02666
Rehan-AF <af.rehan03@gmail.com>
2023-02-02 17:35:04 +0500

    CRUD complete

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,11 +3,33 @@ import {
   createUser,
   getAllUser,
   getUser,
+  updateUser,
+  upload,
 } from '../controllers/userController.js';
+import USER from '../model/userModel.js';
 
 const router = Router();
 
 router.get('/', getAllUser);
 router.get('/:id', getUser);
-router.post('/', createUser);
+router.post('/', upload.single('image'), createUser);
+router.put('/:id', upload.single('image'), updateUser);
+
+// router.post('/upload', upload.single('image'), async (req, res) => {
+//   try {
+//     const imagePath = req.file.filename;
+//     const data = new USER({
+//       image: imagePath,
+//     });
+//     await data.save();
+//     res.status(201).send({
+//       message: 'Image saved successfully',
+//     });
+//   } catch (err) {
+//     res.status(400).json({
+//       status: 'fail',
+//       message: 'error',
+//     });
+//   }
+// });
 export default router;
